7a0822074d39ffb010f68c47e9c4759ef136bc9c
Crowdbotics <team@crowdbotics.com>
2023-04-29 00:26:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "united-laser-engrav-40331",
+  url: "https://united-laser-engrav-40331.botics.co",
+  api: "https://united-laser-engrav-40331.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
